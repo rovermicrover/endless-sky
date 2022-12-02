e139b678439d7f4f187f9c8c6b2515b2013e202d
Andrew Rove <rovermicrover@gmail.com>
2022-12-02 17:27:19 -0600

    Wrong namespace

--- source/GameAction.cpp
+++ source/GameAction.cpp
@@ -423,7 +423,7 @@ GameAction GameAction::Instantiate(map<string, string> &subs, int jumps, int pay
 	return result;
 }
 
-void MissionAction::AddOutfitObjective(const std::map<const Outfit *, int> &outfitObjective) const
+void GameAction::AddOutfitObjective(const std::map<const Outfit *, int> &outfitObjective) const
 {
 	int outfitObjectiveCost = 0;
 	for(auto &it : outfitObjective)
