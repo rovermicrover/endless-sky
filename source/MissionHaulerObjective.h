b1e87bd2def8264bddb4e68b38d2157b6145a3d4
Andrew Rove <rovermicrover@gmail.com>
2022-12-08 21:36:08 -0600

    Fix top comment

--- source/MissionHaulerObjective.h
+++ source/MissionHaulerObjective.h
@@ -1,5 +1,5 @@
-/* Set.h
-Copyright (c) 2014 by Michael Zahniser
+/* MissionHaulerObjective.h
+Copyright (c) 2022 by Michael Zahniser
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
